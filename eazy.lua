3f7e18c49e7e8701d0da61f108ad0543fec84a34
profileRAVY <koslyaralex@gmail.com>
2025-05-05 19:41:19 +0400

    Update eazy.lua

--- eazy.lua
+++ eazy.lua
@@ -1,5 +1,1 @@
---ахахахахахах лошара тупой я знаю что ты зашёл сюда негр
 
---твой гуи сдесь
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ravyyxd/bootstrapper/refs/heads/main/best.lua"))()
